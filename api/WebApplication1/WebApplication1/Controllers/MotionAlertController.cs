dfc57cb672fef090ddf686d1f7eb9c7c671397fd
CanoeCanuck <hendrikreginald@gmail.com>
2025-02-08 20:44:48 -0500

    Added Montion Alert API

---
+++ api/WebApplication1/WebApplication1/Controllers/MotionAlertController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotionAlertController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public MotionAlertController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            SELECT AlertType,Confidence,TimeStamp,Message,ImageURL
+                            FROM MotionAlert
+                            ORDER BY AlertId DESC
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [Route("{id}")]
+        [HttpGet]
+        public JsonResult GetDetail(int id)
+        {
+            string query = @"
+                            SELECT AlertType,Confidence,TimeStamp,Message,ImageURL
+                            FROM MotionAlert
+                            WHERE AlertId = @AlertId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@AlertId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(MotionAlert alert)
+        {
+            string query = @"
+                           insert into dbo.MotionAlert
+                           (AlertType,Confidence,TimeStamp,Message,ImageURL)
+                    values (@AlertType,@Confidence,GETDATE(),@Message,@ImageURL)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@AlertType", alert.camera);
+                    myCommand.Parameters.AddWithValue("@Confidence", alert.Confidence);
+                    myCommand.Parameters.AddWithValue("@Message", alert.Message);
+                    myCommand.Parameters.AddWithValue("@ImageURL", alert.ImageURL);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Models/MotionAlert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MotionAlert
+    {
+        public int AlertId { get; set; }
+        public string camera {get; set; }
+        public decimal Confidence { get; set; }
+
+        public string TimeStamp { get; set; }
+
+        public string  Message { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
